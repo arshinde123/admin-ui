8e07db2b375d956de1bcd75a574245ed75a246c9
Arvind Shinde <arshinde123.2013@gmail.com>
2021-12-16 15:48:04 +0530

    updated components

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import Users from './components/users';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <ToastContainer />
+      <nav className="navbar navbar-light bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand text-light">
+            Admin-UI
+          </a>
+        </div>
+      </nav>
+      <div className="mx-5">
+        <Users />
+      </div>
+    </div>
+  );
+}
+
+export default App;
